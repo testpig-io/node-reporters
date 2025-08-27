a3e30fc3d982748743693f1691e3b6e89bdc86f1
Kerry McKeever <kerry@kerrymckeever.com>
2025-08-27 10:21:24 -0400

    add console log for git-info

--- packages/jest-tests/jest.config.js
+++ packages/jest-tests/jest.config.js
@@ -4,8 +4,6 @@ module.exports = {
     reporters: [
         'default',
         ['../jest-reporter/dist/index.js', {
-            projectId: process.env.TESTPIG_PROJECT_ID || 'test-project',
-            runId: process.env.TESTPIG_RUN_ID || 'test-run'
         }]
     ],
     maxWorkers: '50%'
